18f1c672317decb7f86a071d39592ce7b560bde2
박현규 <phg930421@softzion.com>
2026-05-12 13:32:15 +0900

    feat: 타이틀 변경

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,8 +1,8 @@
 const rawSiteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://monibuk.com";
 
 export const siteUrl = new URL(rawSiteUrl.endsWith("/") ? rawSiteUrl : `${rawSiteUrl}/`);
-export const siteName = "Money Book";
-export const siteTitle = "머니북가계부";
+export const siteName = "머니북가계부";
+export const siteTitle = "머니북가계부 | 무료가계부";
 export const siteDescription =
   "수입, 지출, 저축, 투자 기록을 월별 대시보드와 분석 차트로 관리하는 무료 온라인 개인 가계부 서비스입니다.";
 
